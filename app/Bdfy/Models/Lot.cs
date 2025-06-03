a18ab12e916cc9bb44d8ff94196cf3565ae2d6e5
lucas2mz <lucasandrada2606@gmail.com>
2025-06-02 21:46:56 -0300

    All models 👍

--- app/Bdfy/Models/Lot.cs
+++ app/Bdfy/Models/Lot.cs
@@ -5,26 +5,40 @@ namespace BDfy.Models
 {
     public class Lot : Base // Clase lote
     {
-        [Column]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Lot number must be greater than 0")]
+        [Column("lot_number")]
         public int LotNumber { get; set; }
 
-        [Column]
+        [Required]
+        [StringLength(200, ErrorMessage = "The Description cannot have more than 50 characters")]
+        [Column("description")]
         public string Descritpion { get; set; } = null!;
 
-        [Column]
+        [Required]
+        [StringLength(80, ErrorMessage = "The Details cannot have more than 80 characters")]
+        [Column("details")]
+        public string Details { get; set; } = null!;
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Starting price cannot be negative")]
+        [Column("starting_price")]
         public decimal StartingPrice { get; set; }
 
-        [Column]
+        [Range(0, double.MaxValue, ErrorMessage = "Current price must be greater than 0")]
+        [Column("current_price")]
         public decimal? CurrentPrice { get; set; }
 
-        [Column]
+        [Range(0, double.MaxValue, ErrorMessage = "Ending price must be greater than 0")]
+        [Column("ending_price")]
         public decimal? EndigPrice { get; set; }
 
-        [Column]
+        [Required]
+        [Column("auction_id")]
         public int AuctionId { get; set; }
         public Auction Auction { get; set; } = null!;
 
-        [Column]
+        [Column("winner_id")]
         public int? WinnerId { get; set; }
         public UserDetails? Winner { get; set; }
 
