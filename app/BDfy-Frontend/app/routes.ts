f3a2929e4b0d7cef10f871a205c4fb054bcbe203
Fabrizzio <oviedofabrizzio1998@gmail.com>
2025-06-04 16:46:36 -0300

    Add Header and UserMenu components, implement authentication checks, and enhance login page layout

--- app/BDfy-Frontend/app/routes.ts
+++ app/BDfy-Frontend/app/routes.ts
@@ -1,6 +1,14 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
-    route("login", "routes/login.tsx")
+  // Rutas públicas
+  route("/", "routes/layout.tsx", [
+    index("routes/home.tsx"),            // / (home público)
+    route("login", "routes/login.tsx"),  // /login
+  ]),
+
+  // Rutas protegidas
+  route("/", "routes/protectedlayout.tsx", [
+    route("profile", "routes/profile.tsx"),
+  ]),
 ] satisfies RouteConfig;
