2b7767cf7b085047c07a2eea887beaec1017b4a7
Fabri <oviedofabrizzio1998@gmail.com>
2025-06-13 10:36:31 -0300

    Revert "Feature/auction page"

--- app/BDfy-Frontend/types/types.d.ts
+++
@@ -1,8 +0,0 @@
-import 'jwt-decode';
-
-declare module 'jwt-decode' {
-    export interface JwtPayload {
-        userId: string;  // Asegúrate de que 'userId' sea del tipo adecuado (puede ser 'string' o 'number')
-        // Puedes agregar otras propiedades que tengas en tu JWT si es necesario
-    }
-}
--- app/Bdfy/Configurations/AppSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace BDfy.Configurations
-{
-    public class AppSettings
-    {
-        public string SecretKey { get; set; } = string.Empty; // Propiedad para almacenar la clave secreta
-    }
-}
