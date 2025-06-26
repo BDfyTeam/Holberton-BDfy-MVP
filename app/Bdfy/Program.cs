87a50fc85952e6b231d07099cf6c81a3d36fdb49
Franco-byte <fd.reyes3110@gmail.com>
2025-06-25 21:46:00 -0300

    Update CORS settings and modify database connection username

--- app/Bdfy/Program.cs
+++ app/Bdfy/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "https://localhost:3000", "https://127.0.1:3000", // Frontend
-                "http://localhost:5015", "http://127.0.0.1:5015") // Específicos
+                "http://127.0.0.1:5016", "http://localhost:5016") // Arreglando las cagadas del Fabri
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
